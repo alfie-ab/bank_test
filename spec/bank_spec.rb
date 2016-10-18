f35ebc266c96efe6a101ef801e21d2ddbe4c8208
Alfie <alfie_ab@hotmail.co.uk>
2016-10-18 10:18:11 +0100

    client can now view balance

---
+++ spec/bank_spec.rb
@@ -0,0 +1,14 @@
+require 'bank'
+
+describe Bank do
+
+  it "allows user to check balance" do
+    expect(subject.balance).to eq(0)
+  end
+
+  it "allows user to make a deposit" do
+    subject.deposit(1000)
+    expect(subject.balance).to eq(1000)
+  end
+
+end
