a95e63647e69c96e72e88a1682ccf35b394124f6
Alfie <alfie_ab@hotmail.co.uk>
2016-10-18 10:23:23 +0100

    client can now deposit

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -6,9 +6,14 @@ describe Bank do
     expect(subject.balance).to eq(0)
   end
 
-  it "allows user to make a deposit" do
+  it "allows user to make a withdrawal" do
     subject.withdraw(1000)
     expect(subject.balance).to eq(-1000)
+  end
+
+  it "allows user to make a deposit" do
+    subject.deposit(1000)
+    expect(subject.balance).to eq(1000)
   end
 
 end
