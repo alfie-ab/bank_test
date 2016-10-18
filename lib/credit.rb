509a1b89c7ad895e79c1d94ad4fb8bb11334d9f1
Alfie <alfie_ab@hotmail.co.uk>
2016-10-18 14:23:03 +0100

    very close to printing working (i hope), still issue with string conversion and lining up

--- lib/credit.rb
+++ lib/credit.rb
@@ -7,7 +7,7 @@ class Credit
   attr_reader :transaction
 
   def initialize(amount)
-    @transaction = Time.now.strftime("%d/%m/%Y") + " || " + amount.to_s 
+    @transaction = Time.now.strftime("%d/%m/%Y") + " || " + amount.to_s + " || nil "
   end
 
 end
